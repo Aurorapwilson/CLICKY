db6e0efe8aa94d31b2e3272321248a03bbe86dc2
Princess Wilson <aurorapwilson1@gmail.com>
2019-04-13 14:22:59 -0400

    Updates

---
+++ precache-manifest.8f7a963706b90c65e713a83388202faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69f971b0fa94176bca4d",
+    "url": "/clicky/static/css/main.c30ff948.chunk.css"
+  },
+  {
+    "revision": "69f971b0fa94176bca4d",
+    "url": "/clicky/static/js/main.aaf9035f.chunk.js"
+  },
+  {
+    "revision": "e6823e1d7a53e953bdbe",
+    "url": "/clicky/static/js/runtime~main.d68621ad.js"
+  },
+  {
+    "revision": "d05968c021b6eb8308a5",
+    "url": "/clicky/static/js/2.ee85c083.chunk.js"
+  },
+  {
+    "revision": "f405b33ce6ed9e2ea8f73f3da5d5c67c",
+    "url": "/clicky/index.html"
+  }
+];
